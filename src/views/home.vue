f6fd4b708cf58d8a93688946d37e17196b361d6a
Lior Atar <Liorat18@gmail.com>
2022-11-30 17:22:36 +0200

    home page info style

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,23 +1,24 @@
 <template>
   <section class="home-page">
-      <div class="home-page-info">
-        <div class="info">
-          <h1>Portobello brings all your tasks, teammates, and tools together</h1>
-          <h3>Keep everything in the same place—even if your team isn’t.</h3>
-          <div class="home-page-sign">
+    <div class="home-page-info">
+      <div class="info">
+        <h1>Portobello brings all your tasks, teammates, and tools together</h1>
+        <h3>Keep everything in the same place—even if your team isn’t.</h3>
+        <div class="home-page-sign">
           <input type="email" placeholder="Email">
           <router-link to="/board"><button>Sign up - it`s free!</button></router-link>
-          </div>
-          <router-link to="/board"><button>Start Demo</button></router-link>
         </div>
+        <router-link to="/board" class="btn-start"><button>Start Demo</button></router-link>
       </div>
-      <img src="https://translate.google.com/website?sl=en&tl=iw&hl=iw&prev=search&u=https://images.ctfassets.net/rz1oowkt5gyp/4BOU9h5tEyu4KMlcohLQ8B/53535ddf408664bb7de6367cafed3584/HeroBoard__BoardView_4x.png?w%3D1140%26fm%3Dwebp"/>
+    </div>
+    <img
+      src="https://translate.google.com/website?sl=en&tl=iw&hl=iw&prev=search&u=https://images.ctfassets.net/rz1oowkt5gyp/4BOU9h5tEyu4KMlcohLQ8B/53535ddf408664bb7de6367cafed3584/HeroBoard__BoardView_4x.png?w%3D1140%26fm%3Dwebp" />
     <div class="product">
       <div class="product-info">
         <h2>A productivity powerhouse</h2>
         <p>
-            Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who's 
-            doing what and what needs to get done. Learn more in our guide for getting started.
+          Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who's
+          doing what and what needs to get done. Learn more in our guide for getting started.
         </p>
       </div>
       <div class="product-preview">
@@ -32,31 +33,34 @@ export default {
 }
 </script>
 <style lang="scss">
-.home-page{
-  input{
+.home-page {
+  input {
     width: 200px;
     margin-left: auto;
     margin-right: auto;
   }
 
-  img{
+  img {
     margin-top: 50px;
     max-height: 400px;
     margin-bottom: 100px;
   }
+
   margin-left: auto;
   margin-right: auto;
   text-align: center;
-  &-info{
+
+  &-info {
     display: flex;
     gap: 10px;
     flex-direction: column;
   }
-.product{
-  p{
+
+  .product {
+    p {
       margin-left: auto;
       margin-right: auto;
     }
-}
+  }
 }
 </style>
