f4a9146ea26f20b3dac6dfb30b0dc49e44907730
barzaken <bar.zaken@boyar.org.il>
2022-11-30 10:07:16 +0200

    board-header

---
+++ src/cmps/board-header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="board-header">
+    <div class="board-info">
+      <input type="text">
+      <i class="fa-light fa-star"></i>
+      <select>
+        <option value="board">Board</option>
+      </select>
+      <h5>Filter cmp</h5>
+      <h5>Board memebers</h5>
+      <h5>Share</h5>
+      <h5>Menu</h5>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props:{
+        board:Object
+    },
+    data(){
+        return{
+            currBoard: null
+        }
+    }
+
+}
+</script>
+<style lang=""></style>
