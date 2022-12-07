d0560c068f93b5823e0be22f898c1da0a8747609
barzaken <bar.zaken@boyar.org.il>
2022-12-07 18:38:34 +0200

    added backend!!!!! and sockets!!!!!!!!1

--- src/store/board.store.js
+++ src/store/board.store.js
@@ -1,4 +1,5 @@
-import { boardService } from "../services/board.service.js";
+// import { boardService } from "../services/board.service.js";
+import { boardService } from "../services/board-service-local.js";
 
 export const boardStore = {
   state: {
