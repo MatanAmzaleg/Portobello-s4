a4cbab35e65b272e74a1507b7b25d2787d2d5cbe
Matan <matanamz@gmail.com>
2022-12-08 00:48:21 +0200

    night push

--- src/cmps/board-header.vue
+++ src/cmps/board-header.vue
@@ -2,7 +2,7 @@
   <section v-if="board" class="board-header">
     <div class="left-section">
       <span
-        :style="!calculatedColor?.isDark ? { color: 'black' } : { color: 'white' }"
+        :style="!getCalcColor?.isDark ? { color: 'black' } : { color: 'white' }"
         class="board-name-input"
         role="textbox"
         ref="span"
@@ -29,10 +29,10 @@
       <popper>
         <button
           @click="changeIsModalOpen"
-          :style="!calculatedColor?.isDark ? { color: 'black' } :  { color: 'white' }"
+          :style="!getCalcColor?.isDark ? { color: 'black' } :  { color: 'white' }"
           class="filter-btn"
         >
-        <svg class="filter-icon" :style="!calculatedColor?.isDark ? { color: 'black' } :  { color: 'white' }" width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4.61799 6C3.87461 6 3.39111 6.78231 3.72356 7.44721L3.99996 8H20L20.2763 7.44721C20.6088 6.78231 20.1253 6 19.3819 6H4.61799ZM10.8618 17.7236C10.9465 17.893 11.1196 18 11.309 18H12.6909C12.8803 18 13.0535 17.893 13.1382 17.7236L14 16H9.99996L10.8618 17.7236ZM17 13H6.99996L5.99996 11H18L17 13Z" fill="currentColor"></path></svg>
+        <svg class="filter-icon" :style="!getCalcColor?.isDark ? { color: 'black' } :  { color: 'white' }" width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4.61799 6C3.87461 6 3.39111 6.78231 3.72356 7.44721L3.99996 8H20L20.2763 7.44721C20.6088 6.78231 20.1253 6 19.3819 6H4.61799ZM10.8618 17.7236C10.9465 17.893 11.1196 18 11.309 18H12.6909C12.8803 18 13.0535 17.893 13.1382 17.7236L14 16H9.99996L10.8618 17.7236ZM17 13H6.99996L5.99996 11H18L17 13Z" fill="currentColor"></path></svg>
           Filter
         </button>
         <template #content="{ close }">
@@ -121,7 +121,7 @@
         :memberIds="getBoardMembers"
       ></mini-users-designed>
       <Popper>
-        <el-button :style="!calculatedColor?.isDark ? { color: 'black' } :  { color: 'white' }"
+        <el-button :style="!getCalcColor?.isDark ? { color: 'black' } :  { color: 'white' }"
           >Share</el-button
         >
         <template #content="{ close }">
@@ -132,7 +132,7 @@
 
       <button @click="$emit('openBoardMenu')" class="ellipsis-btn">
         <font-awesome-icon
-          :style="!calculatedColor?.isDark ? { color: 'black' } : { color: 'white' }"
+          :style="!getCalcColor?.isDark ? { color: 'black' } : { color: 'white' }"
           class="ellipsis-icon"
           icon="fa-solid fa-ellipsis"
         />
