1e1814b6d7be729374b6ee3aa55d8310be7bb267
Lior Atar <Liorat18@gmail.com>
2022-12-07 21:00:46 +0200

    fixed member popper

--- src/cmps/popper-member.vue
+++ src/cmps/popper-member.vue
@@ -1,18 +1,15 @@
-<template lang="">
-        <div class="popper-content member-picker">
-          <popperModal title="Members" @closeModal="close"/>
-          <div class="members-section">
-            <input v-focus @input="filterMembers" v-model="input" class="members-input-search" type="search" placeholder="Search Members">
-            <div class="members">
-              <h5 class="board-members-title">Board members</h5>
-              <div class="board-members">
-                <div @click="addMember(member._id)" v-for="member in boardMembers" class="member">
-                  <img class="member-img" :src="member.imgUrl">
-                  <p class="member-name">{{member.fullname}}</p>
-                  <font-awesome-icon v-if="members?.includes(member._id)" class="checkedIcon" icon="fa-solid fa-check" />
-                </div>
-              </div>
-            </div>
+<template>
+  <div class="popper-content member-picker">
+    <popperModal title="Members" @closeModal="close"/>
+    <div class="members-section">
+      <input v-focus @input="filterMembers" v-model="input" class="members-input-search" type="search" placeholder="Search Members">
+      <div class="members">
+        <h5 class="board-members-title">Board members</h5>
+        <div class="board-members">
+          <div @click="addMember(member._id)" v-for="member in boardMembers" class="member">
+            <img class="member-img" :src="member.imgUrl">
+            <p class="member-name">{{member.fullname}}</p>
+            <font-awesome-icon v-if="members?.includes(member._id)" class="checkedIcon" icon="fa-solid fa-check" />
           </div>
         </div>
       </div>
@@ -22,20 +19,20 @@
 <script>
 import popperModal from './popper-modal.vue';
 export default {
-    props:{
-        members: Array
-    },
-    created(){
-      this.boardMembers = this.$store.getters.currBoard.members
-    },
-    data(){
-        return{
-            boardMembers : [],
-            input:''
-        }
-    },
-    methods:{
-      getMemberName(id) {
+  props: {
+    members: Array
+  },
+  created() {
+    this.boardMembers = this.$store.getters.currBoard.members
+  },
+  data() {
+    return {
+      boardMembers: [],
+      input: ''
+    }
+  },
+  methods: {
+    getMemberName(id) {
       const member = this.boardMembers.find((m) => m._id === id);
       return member.fullname;
     },
@@ -53,12 +50,10 @@ export default {
       }
       this.$emit('add-member', this.members)
     },
-    },
   },
   components: {
     popperModal
   }
-
 }
 </script>
 <style lang="">
