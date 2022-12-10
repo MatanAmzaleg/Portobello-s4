e545bf98a5b761dab74260fc8e975555bf4c2282
Lior Atar <Liorat18@gmail.com>
2022-12-11 01:29:40 +0200

    edit

--- src/cmps/task-quick-edit.vue
+++ src/cmps/task-quick-edit.vue
@@ -1,83 +1,170 @@
 <template>
-  <section class="task-list">
-    <div class="cover" v-if="Object.keys(item.style).length !== 0 && item.style?.mode !== 'full'" :style="
-      item.style?.bgColor
-        ? { 'background-color': item.style.bgColor }
-        : {
-          'background-image': 'url( ' + item.style.imgUrl + ')',
-          height: '80px',
-          'background-position': 'center',
-          'background-size': 'cover',
-        }
-    "></div>
-    <div class="cover" v-if="item.style?.mode === 'full'" :style="
-      item.style?.bgColor
-        ? { 'background-color': item.style.bgColor, height: '56px' }
-        : {
-          'background-image': 'url( ' + item.style.imgUrl + ')',
-          height: '256px',
-          'background-position': 'center',
-          'background-size': 'cover',
-        }
-    ">
-    </div>
-    <section class="task-preview">
-      <div v-if="item.style?.mode !== 'full'" class="padded-section">
-        <div class="labels" v-if="item.labelIds?.length > 0">
-          <span @click.stop="openLabelExtended" class="label" :class="labelExtendedClass" v-for="label in item.labelIds"
-            :style="{ 'background-color': labelColor(label) }">
-            <div class="circle" :style="{ 'background-color': labelColor(label) }"></div>
-            <span class="label-title" v-if="isLabelsExtended">{{
-                getLabelTitle(label)
-            }}</span>
-          </span>
+  <section class="quick-edit">
+    <div>
+      <section class="task-list">
+        <div class="cover" v-if="Object.keys(item.style).length !== 0 && item.style?.mode !== 'full'" :style="
+          item.style?.bgColor
+            ? { 'background-color': item.style.bgColor }
+            : {
+              'background-image': 'url( ' + item.style.imgUrl + ')',
+              height: '80px',
+              'background-position': 'center',
+              'background-size': 'cover',
+            }
+        "></div>
+        <div class="cover" v-if="item.style?.mode === 'full'" :style="
+          item.style?.bgColor
+            ? { 'background-color': item.style.bgColor, height: '56px' }
+            : {
+              'background-image': 'url( ' + item.style.imgUrl + ')',
+              height: '256px',
+              'background-position': 'center',
+              'background-size': 'cover',
+            }
+        ">
         </div>
-        <!-- <span class="task-router-link">{{ item.title }}</span> -->
-        <textarea class="textarea">{{ item.title }}</textarea>
-        <div v-if="taskExtra(item)" class="task-preview-info">
-          <div class="task-preview-content">
-            <span v-if="item.isWatched" class="watch-icon"></span>
-            <TaskDatePreview v-if="item.dueDate" :status="item.status" :date="item.dueDate" :taskId="item.id" />
-            <span v-if="item.description?.length" class="description-icon"></span>
-            <div v-if="item.comments.length > 0" class="task-preview-checklist">
-              <span class="comments-icon">
+        <section class="task-preview">
+          <div v-if="item.style?.mode !== 'full'" class="padded-section">
+            <div class="labels" v-if="getTaskLabels?.length > 0">
+              <span @click.stop="openLabelExtended" class="label" :class="labelExtendedClass"
+                v-for="label in getTaskLabels" :style="{ 'background-color': labelColor(label) }">
+                <div class="circle" :style="{ 'background-color': labelColor(label) }"></div>
+                <span class="label-title" v-if="isLabelsExtended">{{
+                    getLabelTitle(label)
+                }}</span>
               </span>
-              <span class="comments-span">{{ item.comments.length }}</span>
             </div>
-            <span v-if="item.attachments" class="attachment-icon">{{
-                item.attachments.length
-            }}</span>
-            <div class="task-preview-checklist" :class="isDone ? 'done' : ''">
-              <span v-if="item.checklists?.length" class="checklist-icon">
-                <span>{{ getTodoStatus(item.checklists) }}</span>
-              </span>
+            <!-- <span class="task-router-link">{{ item.title }}</span> -->
+            <textarea class="textarea">{{ item.title }}</textarea>
+            <div v-if="taskExtra(item)" class="task-preview-info">
+              <div class="task-preview-content">
+                <span v-if="item.isWatched" class="watch-icon"></span>
+                <TaskDatePreview v-if="item.dueDate" :status="item.status" :date="item.dueDate" :taskId="item.id" />
+                <span v-if="item.description?.length" class="description-icon"></span>
+                <div v-if="item.comments.length > 0" class="task-preview-checklist">
+                  <span class="comments-icon">
+                  </span>
+                  <span class="comments-span">{{ item.comments.length }}</span>
+                </div>
+                <span v-if="item.attachments" class="attachment-icon">{{
+                    item.attachments.length
+                }}</span>
+                <div class="task-preview-checklist" :class="isDone ? 'done' : ''">
+                  <span v-if="item.checklists?.length" class="checklist-icon">
+                    <span>{{ getTodoStatus(item.checklists) }}</span>
+                  </span>
+                </div>
+              </div>
+              <mini-users-designed :memberIds="item.memberIds"></mini-users-designed>
             </div>
           </div>
-          <mini-users-designed :memberIds="item.memberIds"></mini-users-designed>
-        </div>
-      </div>
-    </section>
+        </section>
+      </section>
+      <button class="save-btn">Save</button>
+    </div>
+    <div class="actions-quick">
+      <button class="button-quick-edit">
+        <span class="header-icon icon-actions"></span>
+        Open card
+      </button>
+      <Popper @open:popper="addFocus">
+        <button class="button-quick-edit">
+          <span class="label-icon icon-actions"></span>
+          Edit labels
+        </button>
+        <template #content="{ close }">
+          <div class="quick-popper-left">
+            <popperLabel @closeModal="close" @updateBoard="updateBoard" @saveLabel="saveLabel"
+            :labelIds="getTaskLabels" />
+          </div>
+        </template>
+      </Popper>
+      <Popper @open:popper="addFocus">
+        <button class="button-quick-edit">
+          <span class="icon-actions member-icon"></span>
+          Change Members
+        </button>
+        <template #content="{ close }">
+          <div class="quick-popper-left">
+            <popperMember @addMember="addMember" @closeModal="close" :members="getTaskMembers" />
+          </div>
+        </template>
+      </Popper>
+      <Popper @open:popper="addFocus">
+        <button class="button-quick-edit">
+          <span class="card-cover-icon icon-actions"></span>
+          Change cover
+        </button>
+        <template #content="{ close }">
+          <div class="cover-popper quick-popper-left">
+            <popperCover @closeModal="close" @setCover="setCover" :style="style" />
+          </div>
+        </template>
+      </Popper>
+      <Popper @open:popper="addFocus">
+        <button class="button-quick-edit">
+          <svg class="icon-actions" width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M13 6C13 5.44772 12.5523 5 12 5C11.4477 5 11 5.44772 11 6V12C11 12.2652 11.1054 12.5196 11.2929 12.7071L13.7929 15.2071C14.1834 15.5976 14.8166 15.5976 15.2071 15.2071C15.5976 14.8166 15.5976 14.1834 15.2071 13.7929L13 11.5858V6Z"
+              fill="currentColor"></path>
+            <path fill-rule="evenodd" clip-rule="evenodd"
+              d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"
+              fill="currentColor"></path>
+          </svg>
+          Edit dates
+        </button>
+        <template #content="{ close }">
+          <div class="quick-popper-left">
+            <popper-calendar @closeModal="close" @removeDate="removeDate" @saveDate="saveDate" />
+          </div>
+        </template>
+      </Popper>
+      <button class="button-quick-edit">
+        <span class="archive-icon icon-actions"></span>
+        Archive
+      </button>
+    </div>
   </section>
 </template>
 <script>
-import popperAddMember from "./popper-add-member.vue";
 import popperCalendar from "./popper-calendar.vue";
-import popperLabel from "./popper-label.vue";
 import miniUsersDesigned from "./mini-users-designed.vue";
 import miniUsers from "./mini-users.vue";
 import TaskDatePreview from "./task-date-preview.vue";
+import popperMember from "./popper-member.vue";
+import popperLabel from "./popper-label.vue";
+import popperCover from "./popper-cover.vue";
+
+// export const functionsTaskDetails = {
+//   saveTaskMembers,
+//   getTaskMembers,
+//   updateBoard,
+//   saveTaskLabels,
+//   getTaskLabels,
+//   getTaskDate,
+//   saveTaskDate,
+//   removeTaskDate,
+//   getTaskStyle,
+//   saveTaskCover,
+//   deleteTask,
+// }
+
 export default {
   props: {
     item: Object,
   },
   data() {
     return {
-      taskTitle: ''
+      task: {},
+      taskTitle: '',
+      currBoard: {}
     };
   },
   created() {
     this.taskTitle = this.item?.title
-    console.log('1', this.item)
+    this.task = this.item
+    this.currBoard = this.getCurrBoard
   },
   methods: {
     labelColor(id) {
@@ -85,6 +172,7 @@ export default {
         return l.id === id;
       });
       if (!label) return;
+      console.log('label.color', label.color)
       return label.color;
     },
     labelText(id) {
@@ -127,15 +215,76 @@ export default {
     },
     updateQuickEdit() {
       this.$emit('updateQuickEdit', item.id)
-    }
+    },
+    async updateBoard(board) {
+      console.log('newBoard', board)
+      await this.$store.dispatch({ type: "updateBoard", board });
+    },
+    async updateTask() {
+      try {
+        let board = JSON.parse(JSON.stringify(this.currBoard));
+        let taskIdx;
+        let groupIdx = board.groups.findIndex((group) =>
+          group.tasks.some((task, idx) => {
+            if (task.id === this.task.id) taskIdx = idx;
+            return task.id === this.task.id;
+          })
+        );
+        board.groups[groupIdx].tasks[taskIdx] = this.task;
+        if (this?.task?.lastActivity)
+          board.activities.unshift(this.task.lastActivity);
+        await this.$store.dispatch({ type: "updateBoard", board });
+      } catch (err) {
+        console.log("cant Update task", err);
+      }
+    },
+    addMember({ members, msg }) {
+      console.log('test');
+      this.task.memberIds = members;
+      this.task.lastActivity = {
+        msg: msg + this.task.title,
+        byMember: this.$store.getters.loggedinUser,
+        date: Date.now(),
+      };
+      this.updateTask()
+    },
+    saveLabel({ labels, msg }) {
+      console.log('entered!', labels)
+      console.log(labels, msg);
+      this.task.labelIds = labels;
+      this.task.lastActivity = {
+        msg: msg + this.task.title,
+        byMember: this.$store.getters.loggedinUser,
+        date: Date.now(),
+      };
+      this.updateTask();
+    },
+  },
+  computed: {
+    getTaskLabels() {
+      const labels = JSON.parse(JSON.stringify(this.task.labelIds))
+      console.log('all Labels', labels)
+      return labels;
+    },
+    getTaskMembers() {
+      const members = JSON.parse(JSON.stringify(this.task.memberIds))
+      return members;
+    },
+    getTaskDate() {
+      return this.task.dueDate;
+    },
+    getCurrBoard() {
+      return this.$store.getters.currBoard;
+    },
   },
   components: {
-    popperAddMember,
     popperCalendar,
     popperLabel,
     miniUsersDesigned,
     miniUsers,
-    TaskDatePreview
+    TaskDatePreview,
+    popperMember,
+    popperCover
   },
 };
 </script>
