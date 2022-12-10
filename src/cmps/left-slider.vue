6aac156d9fdbf7a8ac86e425fb3c9d2c2d8f4c90
matan amzaleg <matanamz@gmail.com>
2022-12-10 20:27:47 +0200

    cvhanges

--- src/cmps/left-slider.vue
+++ src/cmps/left-slider.vue
@@ -37,7 +37,7 @@
         </button>
       </div>
       <ul class="nav-item-content">
-        <li v-for="board in getBoards" :style="getCurrBoard._id === board._id ? {'background-color' : 'rgba(208, 208, 208, 0.395)'} :  {'background-color' : 'inherit'}">
+        <li v-for="board in boards" :style="getCurrBoard?._id === board._id ? {'background-color' : 'rgba(208, 208, 208, 0.395)'} :  {'background-color' : 'inherit'}">
           <div class="starred-board-container" @click="moveToBoard(board)">
             <img
               v-if="board.style?.imgUrl"
@@ -64,6 +64,14 @@
 
 <script>
 export default {
+    data(){
+        return{
+            boards:null
+        }
+    },
+    created(){
+        this.boards = this.getBoards 
+    },
     methods: {
         moveToBoard(board) {
       console.log("move board");
