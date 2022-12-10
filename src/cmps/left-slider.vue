5c6730760fd8d5dbb746afccb6cb3e05d37f82ec
matan amzaleg <matanamz@gmail.com>
2022-12-10 20:04:35 +0200

    cvhanges

--- src/cmps/left-slider.vue
+++ src/cmps/left-slider.vue
@@ -17,7 +17,7 @@
     </div>
     <hr />
     <div class="first-section">
-      <div class="boards flex">
+      <div @click="routeToBoardPage" class="boards flex">
         <font-awesome-icon class="icon" icon="fa-brands fa-trello" />
         <p>Boards</p>
       </div>
@@ -36,10 +36,51 @@
           <font-awesome-icon class="icon-plus-bigger" icon="fa-solid fa-plus" />
         </button>
       </div>
-
+      <ul class="nav-item-content">
+        <li v-for="board in getBoards" :style="getCurrBoard._id === board._id ? {'background-color' : 'rgba(208, 208, 208, 0.395)'} :  {'background-color' : 'rgba(208, 208, 208, 0.395)'}">
+          <div class="starred-board-container" @click="moveToBoard(board)">
+            <img
+              v-if="board.style?.imgUrl"
+              class="style-container"
+              :src="board.style?.imgUrl"
+              alt=""
+            />
+            <div
+              v-if="board.style?.bgColor"
+              class="style-container"
+              :style="{ 'background-color': board.style?.bgColor }"
+              alt=""
+            ></div>
+            <div class="board-name-container">
+              <label class="title-1">{{ board.title }}</label>
+            </div>
+          </div>
+        </li>
+      </ul>
       <!-- PUT BOARD LIST HERE -->
     </div>
   </section>
 </template>
 
-
+<script>
+export default {
+    methods: {
+        moveToBoard(board) {
+      console.log("move board");
+      this.$store.dispatch({ type: "setCurrBoard", boardId: board._id });
+      this.$router.push(`/board/${board._id}`);
+    },
+    routeToBoardPage(){
+        this.$router.push('/board')
+    }
+    },
+  computed: {
+    getBoards() {
+      return this.$store.getters.boards;
+    },
+    getCurrBoard(){
+        return this.$store.getters.currBoard;
+    }
+  },
+};
+</script>
